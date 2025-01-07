49617b8f9b40bfe2f7add96599806492fc991bb5
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-07 20:59:06 +0100

    Add associations and validations to the Market Research model

--- app/models/market_research.rb
+++ app/models/market_research.rb
@@ -1,3 +1,10 @@
 class MarketResearch < ApplicationRecord
+
   belongs_to :project
+  validates :market_size, numericality: { greater_than: 0 }
+  validates :competitors, presence: true
+  validates :target_audience, presence: true
+  
+  serialize :opportunities, JSON
+  serialize :pain_points, JSON
 end
