c9b34491c6054a162ecccdc2227417bf4b47fbd1
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-07 19:46:43 +0100

    Add the project dev model

---
+++ app/models/development_project.rb
@@ -0,0 +1,3 @@
+class DevelopmentProject < ApplicationRecord
+  belongs_to :project
+end
---
+++ db/migrate/20250107184520_create_development_projects.rb
@@ -0,0 +1,14 @@
+class CreateDevelopmentProjects < ActiveRecord::Migration[8.0]
+  def change
+    create_table :development_projects do |t|
+      t.references :project, null: false, foreign_key: true
+      t.string :phase
+      t.date :timeline
+      t.decimal :budget, precision: 10, scale: 2
+      t.string :tech_stack
+      t.text :milestones
+
+      t.timestamps
+    end
+  end
+end
