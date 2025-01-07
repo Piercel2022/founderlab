78264adb8a8d34269aba2ef6c26618ff9f3203ad
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-07 21:02:33 +0100

    Add associations and validations to the Mentor model

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,3 +1,10 @@
 class Mentor < ApplicationRecord
+
   belongs_to :user
+  has_many :meetings
+  
+  validates :expertise, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  
+  serialize :expertise, Array
 end
