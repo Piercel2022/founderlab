3236f5b80a7b2abdcae4c8c8d76aa3100c44491b
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-07 20:13:01 +0100

    Add the Meeting model

---
+++ db/migrate/20250107191211_create_meetings.rb
@@ -0,0 +1,14 @@
+class CreateMeetings < ActiveRecord::Migration[8.0]
+  def change
+    create_table :meetings do |t|
+      t.references :mentor, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.datetime :date
+      t.string :status
+      t.text :notes
+      t.string :meeting_type
+
+      t.timestamps
+    end
+  end
+end
