87711283d4fc881b5122f52bfcb7d76110d97d65
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-07 20:19:06 +0100

    Add the Resource model

---
+++ db/migrate/20250107191830_create_resources.rb
@@ -0,0 +1,13 @@
+class CreateResources < ActiveRecord::Migration[8.0]
+  def change
+    create_table :resources do |t|
+      t.string :title
+      t.text :description
+      t.string :category
+      t.string :file_url
+      t.string :access_level
+
+      t.timestamps
+    end
+  end
+end
