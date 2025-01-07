e262de100fa49e0cd83ed254023d5d0650cffc49
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-07 21:05:43 +0100

    Add associations and validations to the Resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,9 @@
 class Resource < ApplicationRecord
+    
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :file_url, presence: true
+  
+  enum access_level: { free: 0, premium: 1, enterprise: 2 }
+  enum category: { document: 0, video: 1, template: 2, tool: 3 }
 end
