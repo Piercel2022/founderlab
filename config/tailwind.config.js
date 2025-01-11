a009b700ceb0834e827654fdaa4cb37634485885
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-11 23:33:56 +0100

    Add the tailwind config file

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,11 +2,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
+   './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/assets/stylesheets/**/*.scss',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
