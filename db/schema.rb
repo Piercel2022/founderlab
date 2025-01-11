43dc04ec080a07dc4f3ecb69af68e7f2f61b177f
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-11 23:42:24 +0100

    Add the workflow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_10_151038) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_11_111847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -174,6 +174,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_151038) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
     t.index ["slug"], name: "index_startups_on_slug", unique: true
   end
 
@@ -221,7 +222,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_151038) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.integer "status", default: 0, null: false
-    t.string "role"
+    t.string "role", default: "viewer"
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
