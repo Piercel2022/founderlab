2ba814d05fdc1804cb665f9bef6255dc3c195ada
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-15 19:10:56 +0100

    Add the activity log model

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -1,4 +1,8 @@
 class ActivityLog < ApplicationRecord
-  belongs_to :trackable
   belongs_to :user
+  belongs_to :trackable, polymorphic: true
+  
+  validates :action, presence: true
+  
+  scope :recent, -> { order(created_at: :desc).limit(10) }
 end
