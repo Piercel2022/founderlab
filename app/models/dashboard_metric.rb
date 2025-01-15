5384a6ae0087cda4051be9d757fc3e19a2364ea0
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-15 19:11:50 +0100

    Add the dashboard metrics

--- app/models/dashboard_metric.rb
+++ app/models/dashboard_metric.rb
@@ -1,3 +1,8 @@
 class DashboardMetric < ApplicationRecord
   belongs_to :user
+  
+  validates :name, presence: true
+  validates :category, presence: true
+  
+  scope :by_category, ->(category) { where(category: category) }
 end
