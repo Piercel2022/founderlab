ba924b22c6aeac9b3f674fd4879bc24fc7da155c
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-17 20:14:41 +0100

    Add the workflow

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,8 +26,8 @@ class User < ApplicationRecord
 
   # Validations
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 },
-            format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows letters, numbers, and underscores" }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }, 
+  format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows letters, numbers, and underscores" }
   validate :password_complexity
   validate :prevent_password_reuse
   validate :avatar_content_type
